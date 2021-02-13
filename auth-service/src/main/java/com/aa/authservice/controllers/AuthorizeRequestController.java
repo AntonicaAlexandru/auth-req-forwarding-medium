e40a3450cde69c8c7402e14ad89ad608e5414945
ALex Antonica <aantonica@veridiumid.com>
2021-02-13 17:13:28 +0200

    final commit, i hope

--- auth-service/src/main/java/com/aa/authservice/controllers/AuthorizeRequestController.java
+++ auth-service/src/main/java/com/aa/authservice/controllers/AuthorizeRequestController.java
@@ -1,6 +1,8 @@
 package com.aa.authservice.controllers;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,17 +20,25 @@ public class AuthorizeRequestController {
 
     private static final String TEST_HEADER = "test-header";
     @GetMapping("/request")
-    public void authorizeRequest(HttpServletRequest request) throws Exception {
+    public ResponseEntity authorizeRequest(HttpServletRequest request) throws Exception {
 
         log.info("Started checking the request");
         // For testing purposes
         String header = request.getHeader(TEST_HEADER);
         if (isNull(header)) {
-            log.info("Request failed the check, throwing 500");
-            throw new Exception("Throw me a 500");
+            log.info("Request failed the check, throwing 300");
+            return new ResponseEntity("Can't touch this.", HttpStatus.FORBIDDEN);
         }
 
         log.info("Request passed the check, returning 200");
+        return new ResponseEntity("Proceed milord", HttpStatus.OK);
     }
 
 }
+
+aantonica:auth-req-forwarding-medium alexantonica$ kubectl get svc
+        NAME           TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)          AGE
+        auth-service   NodePort    10.101.192.176   <none>        9998:30735/TCP   80m
+        doc-service    NodePort    10.96.127.182    <none>        9999:31458/TCP   80m
+        kubernetes     ClusterIP   10.96.0.1        <none>        443/TCP          3h43m
+        aantonica:auth-req-forwarding-medium alexantonica$
--- doc-service/src/main/java/com/aa/docservice/controllers/DocController.java
+++ doc-service/src/main/java/com/aa/docservice/controllers/DocController.java
@@ -1,5 +1,6 @@
 package com.aa.docservice.controllers;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +12,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/doc")
+@Log4j2
 public class DocController {
 
     @GetMapping("/fetch")
     public ResponseEntity<Map<String, String>> getDoc() {
 
+        log.info("Received fetching doc request");
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("test", "value");
+
+        log.info("Returning doc");
         return new ResponseEntity<>(hashMap, HttpStatus.OK);
     }
 }
